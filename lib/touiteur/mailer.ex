9059fdc18b232af31ee46fadf4b0762e4b97f53e
Pierre Le Gall <pierre@legall.im>
2022-12-03 15:34:22 +0100

    Credo installed for static code analysis
    The Credo config is the vanilla generated config.
    
    Commands history:
    $ mix credo.gen.config

--- lib/touiteur/mailer.ex
+++ lib/touiteur/mailer.ex
@@ -1,3 +1,5 @@
 defmodule Touiteur.Mailer do
+  @moduledoc false
+
   use Swoosh.Mailer, otp_app: :touiteur
 end
--- lib/touiteur_web/components/layouts.ex
+++ lib/touiteur_web/components/layouts.ex
@@ -1,4 +1,6 @@
 defmodule TouiteurWeb.Layouts do
+  @moduledoc false
+
   use TouiteurWeb, :html
 
   embed_templates "layouts/*"
